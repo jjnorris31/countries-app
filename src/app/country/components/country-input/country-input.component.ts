8f80291934ed52a85bdd72d6972d1380525e6648
jjnorris31 <jjnorris31@gmail.com>
2022-01-26 12:58:08 -0600

    feat: added debounced Subject to country input component

--- src/app/country/components/country-input/country-input.component.ts
+++ src/app/country/components/country-input/country-input.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators'
 
 @Component({
   selector: 'app-country-input',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountryInputComponent implements OnInit {
 
+  @Output() onEnter: EventEmitter<string> = new EventEmitter();
+  
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+
+  public term: string = "";
+
+  public debounced: Subject<string> = new Subject();
+
   constructor() { }
 
   ngOnInit(): void {
+    this.debounced
+    .pipe(debounceTime(300))
+    .subscribe(valor => {
+      this.onEnter.emit(this.term);
+    })
+  }
+
+  public handleSubmit() {
+    this.onEnter.emit(this.term);
+  }
+
+  public keyPressed() {
+    this.debounced.next(this.term)
   }
 
 }
