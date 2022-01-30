394aee9290e12a1d99fa2d28e4a36491891f0435
jjnorris31 <jjnorris31@gmail.com>
2022-01-30 11:19:59 -0600

    feat: added template elements in by region component

--- src/app/country/pages/by-region/by-region.component.ts
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ByRegionComponent implements OnInit {
 
+  public regions: string[] = ['EU', 'EFTA', 'CARICOM', 'PA', 'AU', 'USAN', 'EEU', 'AL', 'ASEAN', 'CAIS', 'CEFTA', 'NAFTA', 'SAARC'];
+  public activeRegion: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public setActiveRegion(region: string) {
+    this.activeRegion = region;
   }
 
 }
